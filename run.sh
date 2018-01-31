61f3e5c1d8e07dfe0ddce2bab519c7d0fca4e728
hlwen <helainwen@hotmail.com>
2018-01-31 18:25:21 +0800

    添加docker文件

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+chown www-data:www-data /app -R
+
+mv /app/data/conf/database.php.back /app/data/conf/database.php
+
+
+if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
+    unset ALLOW_OVERRIDE
+else
+    sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+    a2enmod rewrite
+fi
+source /etc/apache2/envvars
+tail -F /var/log/apache2/* &
+exec apache2 -D FOREGROUND
