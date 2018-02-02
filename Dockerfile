b1563b6e9162d078dd4185ef9a6c682f096f2d39
hlwen <helainwen@hotmail.com>
2018-02-02 17:58:25 +0800

    安装git

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM php:7.0-apache
 
+RUN apt-get update && apt-get -y install
+
 ADD . /app
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ chown www-data:www-data /app -R
 
 #如果文件夹不存在，创建文件夹
 if [ ! -d "/app/waihui" ]; then
- # cd /app && git clone $WAIHUI
+  cd /app && git clone $WAIHUI
 fi
 
 #if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
@@ -16,4 +16,4 @@ fi
 #fi
 #source /etc/apache2/envvars
 #tail -F /var/log/apache2/* &
-exec apache2 -D FOREGROUND
+#exec apache2 -D FOREGROUND
