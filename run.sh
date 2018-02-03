6716119904f4a13607577b1994d2f4d0850a8c9b
hlwen <helainwen@hotmail.com>
2018-02-03 11:46:24 +0800

    添加权限

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-chown www-data:www-data /app -R
+
 
 #mv /app/data/conf/database.php.back /app/data/conf/database.php
 #如果文件夹不存在，创建文件夹
 if [ ! -d "/app/waihui" ]; then
-  cd /app && git clone $WAIHUI && git pull
+  cd /app && git clone $WAIHUI && cd waihui && git pull
 else
-  cd /app/waihui && git branch --set-upstream-to=origin/ master && git pull
+  cd /app/waihui && git branch --set-upstream-to=origin/ master && cd waihui && git pull
 fi
  
 cp /app/waihui/.env.production /app/waihui/.evn
 cp /app/waihui/waihui.conf /etc/apache2/sites-available/
 cp /app/waihui/waihui.conf /etc/apache2/sites-enabled/ 
 
+chown www-data:www-data /app -R
 #if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
 #    unset ALLOW_OVERRIDE
 #else
