df0daee9964a2f4c8660386bf274d943f398a907
hlwen <helainwen@hotmail.com>
2018-02-03 09:49:20 +0800

    安装vim

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM php:7.0-apache
 RUN apt-get update && apt-get -y install git vim
 
 ADD . /app
+
+ENTRYPOINT ["sh","/app/run.sh"]
--- run.sh
+++ run.sh
@@ -2,18 +2,23 @@
 chown www-data:www-data /app -R
 
 #mv /app/data/conf/database.php.back /app/data/conf/database.php
-
 #如果文件夹不存在，创建文件夹
 if [ ! -d "/app/waihui" ]; then
   cd /app && git clone $WAIHUI
+else
+  cd /app/waihui && git reset –hard origin/master && git pulll
 fi
+ 
+cp /app/waihui/.env.production /app/waihui/.evn
+cp /app/waihui/waihui.conf /etc/apache2/sites-available/
+cp /app/waihui/waihui.conf /etc/apache2/sites-enabled/ 
 
-#if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
-#    unset ALLOW_OVERRIDE
-#else
-#    sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
-#    a2enmod rewrite
-#fi
+if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
+    unset ALLOW_OVERRIDE
+else
+    sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+    a2enmod rewrite
+fi
 #source /etc/apache2/envvars
 #tail -F /var/log/apache2/* &
-#exec apache2 -D FOREGROUND
+exec apache2 -D FOREGROUND
