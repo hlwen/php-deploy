23ea00dda54bffd5ac1401547cf3de23a4a3ee43
hlwen <helainwen@hotmail.com>
2018-02-03 14:46:17 +0800

    安装php依赖

--- run.sh
+++ run.sh
@@ -4,15 +4,15 @@ chown www-data:www-data /var/www/html -R
 #mv /app/data/conf/database.php.back /app/data/conf/database.php
 #如果文件夹不存在，创建文件夹
 if [ ! -d "/var/www/html/waihui" ]; then
-  cd /var/www/html && git clone $WAIHUI && cd waihui && git pull
+  cd /var/www/html && git clone $WAIHUI && cd waihui && git pull && composer install
 else
-  cd /var/www/html/waihui && git branch --set-upstream-to=origin/ master && cd waihui && git pull
+  cd /var/www/html/waihui && git pull && cd waihui && git pull && composer install
 fi
  
 cp /var/www/html/waihui/.env.production /var/www/html/waihui/.evn
 cp /var/www/html/waihui/waihui.conf /etc/apache2/sites-available/
 cp /var/www/html/waihui/waihui.conf /etc/apache2/sites-enabled/ 
-cp /var/www/html/waihui/waihui.conf /etc/apache2/sites-enabled/ 
+cp /var/www/html/waihui/waihui.conf /etc/apache2/conf-enabled/ 
 
 
 if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
